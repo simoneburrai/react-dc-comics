1ea64c916aeab338655305fec3b3522cdb3068af
simoneburrai <burrai.s98@gmail.com>
2025-04-03 15:24:24 +0200

    feat: footer html completed

--- src/components/footer-components/FooterBottom.jsx
+++ src/components/footer-components/FooterBottom.jsx
@@ -1,5 +1,20 @@
+import facebookImg from "../../assets/img/fb-logo.png"
+import twitterImg from "../../assets/img/twitter-logo.png"
+import youtubeImg from "../../assets/img/yt-logo.png"
+import pinterestImg from "../../assets/img/pinterest-logo.png"
+import geoImg from "../../assets/img/geo-logo.png"
+
 function FooterBottom() {
-
+    return <div className="footer-bottom">
+        <button>Sign Up Now</button>
+        <div className="links">
+            <span>Follow Us</span>
+            <img src={facebookImg} alt="facebook logo" />
+            <img src={twitterImg} alt="twitter-logo" />
+            <img src={youtubeImg} alt="youtube logo" />
+            <img src={pinterestImg} alt="pinterest logo" />
+            <img src={geoImg} alt="geo logo" />
+        </div>
+    </div>
 }
-
 export default FooterBottom;
